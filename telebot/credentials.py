32ef623588670f1f7fdca3e97965cdca20eb445f
Lim Jun Jie <ljj98855221@hotmail.com>
2021-06-01 14:28:12 +0800

    changed credentials

--- telebot/credentials.py
+++ telebot/credentials.py
@@ -1,3 +1,3 @@
-bot_token = "here goes your access token from BotFather"
-bot_user_name = "the username you entered"
+bot_token = "1890951181:AAE7jafAINXxR1GZ-1TRA_a2r2Ip0h13Z0s"
+bot_user_name = "SoulZero_bot"
 URL = "https://git.heroku.com/soul-zero.git"
