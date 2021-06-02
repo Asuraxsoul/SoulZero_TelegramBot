520401c60c11add08c9c08f5778c37e708ea1a17
Lim Jun Jie <ljj98855221@hotmail.com>
2021-06-02 23:09:47 +0800

    add bot description

--- app.py
+++ app.py
@@ -18,6 +18,8 @@ help_message = """/place to enquire Singapore's bouldering gyms categorized by l
 welcome_message = "Welcome to " + BOTNAME + ", this bot will help you to find a bouldering gym in Singapore!\n" + \
                   help_message
 error_message = """Bot does not understand your input, please try typing /help for help"""
+bot_description = """Want to find a place to boulder in Singapore?
+Fret not, I am here to help!"""
 
 # start the flask app
 app = Flask(__name__)
@@ -36,16 +38,30 @@ def respond():
     # for debugging purposes only
     print("got text message :", text)
 
+    # Telegram bot description
+    bot.set_chat_description(chat_id=chat_id, description=bot_description)
+
     # the first time you chat with the bot AKA the welcoming message
     if text == "/start":
         # send the welcoming message
         bot.sendMessage(chat_id=chat_id, text=welcome_message)
 
     elif text == "/help":
-        bot.sendMessage(chat_id=chat_id, text=help_message, reply_to_message_id=msg_id)
+        bot.sendMessage(chat_id=chat_id, text=help_message)
+
+    elif text == "/feedback":
+        bot.sendMessage(chat_id=chat_id, text="Please type in your feedback")
+        # email me the response
+        bot.sendMessage(chat_id=chat_id, text="Thank you, your feedback has been recorded!")
 
     elif text == "/places":
-        bot.sendMessage(chat_id=chat_id, text=help_message, reply_to_message_id=msg_id)
+        bot.sendMessage(chat_id=chat_id, text="Which part of Singapore are you looking at?")
+        # activate choice
+        # if north, south, east, west, central
+        # then send the whole list of bouldering gyms
+
+    elif text == "/nearme":
+        bot.sendMessage(chat_id=chat_id, text="You are at xxx now, the nearest gyms (within 10km, if any) are xxx.")
 
     else:
         try:
