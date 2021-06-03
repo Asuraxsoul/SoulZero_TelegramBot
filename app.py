7d2481582aeb49b3df3ff77888497d16cac6843c
Lim Jun Jie <ljj98855221@hotmail.com>
2021-06-03 20:54:53 +0800

    inlinekeyboardbutton test2

--- app.py
+++ app.py
@@ -58,8 +58,8 @@ def respond():
         bot.sendMessage(chat_id=chat_id, text=help_message)
 
     elif text == "/places":
-        keyboard = [[telegram.InlineKeyboardButton('/command1')],
-                    [telegram.InlineKeyboardButton('/command2')]]
+        keyboard = [[telegram.InlineKeyboardButton('/command1', callback_data='1')],
+                    [telegram.InlineKeyboardButton('/command2', callback_data='2')]]
         reply_markup = telegram.InlineKeyboardMarkup(keyboard)
 
         bot.sendMessage(chat_id=chat_id, text="Which part of Singapore are you looking at?", reply_markup=reply_markup)
