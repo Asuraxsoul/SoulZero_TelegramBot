fc460031e214b6d38075de9f03f809dab3efa27a
Lim Jun Jie <ljj98855221@hotmail.com>
2021-06-04 20:51:15 +0800

    search gym name function

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ help_message = """/places to enquire Singapore's bouldering gyms categorized by 
 /help to enquire on available commands."""
 welcome_message = "Welcome to Boulder_SG @" + BOTNAME + ", this bot will help you to find a bouldering gym in " \
                   "Singapore!\n\n" + help_message
-error_message = """Bot does not understand your input, please try typing /help for help"""
+error_message = """Bot does not understand your input, please try typing /help to view commands"""
 
 # start the flask app
 app = Flask(__name__)
@@ -72,9 +72,8 @@ def respond():
 
         bot.sendMessage(chat_id=chat_id, text="Which part of Singapore are you looking at?", reply_markup=reply_markup)
         # activate choice
-        # if north, south, east, west, central
-        # then send the whole list of bouldering gyms
 
+    # if north, south, east, west or central, then send the whole list of bouldering gyms -----------------------------
     elif text == "North":
         keyboard = [[]]
         global all_boulder_places
@@ -85,12 +84,13 @@ def respond():
             keyboard_index = keyboard_index + 1
 
         reply_markup = telegram.ReplyKeyboardMarkup(keyboard)
-        bot.sendMessage(chat_id=chat_id, text="Here are the bouldering gyms located at the North",
+        bot.sendMessage(chat_id=chat_id,
+                        text="Here are the bouldering gyms located at the North\n/places to find other gyms",
                         reply_markup=reply_markup)
 
     elif text == "South":
         keyboard = [[]]
-        all_boulder_places = json.loads(boulder_gyms)
+        global all_boulder_places
         keyboard_index = 0
         for gym_info in all_boulder_places['boulderGyms']:
             if gym_info['category'] == 'South':
@@ -98,12 +98,13 @@ def respond():
             keyboard_index = keyboard_index + 1
 
         reply_markup = telegram.ReplyKeyboardMarkup(keyboard)
-        bot.sendMessage(chat_id=chat_id, text="Here are the bouldering gyms located at the South",
+        bot.sendMessage(chat_id=chat_id,
+                        text="Here are the bouldering gyms located at the South\n/places to find other gyms",
                         reply_markup=reply_markup)
 
     elif text == "East":
         keyboard = [[]]
-        all_boulder_places = json.loads(boulder_gyms)
+        global all_boulder_places
         keyboard_index = 0
         for gym_info in all_boulder_places['boulderGyms']:
             if gym_info['category'] == 'East':
@@ -111,12 +112,13 @@ def respond():
             keyboard_index = keyboard_index + 1
 
         reply_markup = telegram.ReplyKeyboardMarkup(keyboard)
-        bot.sendMessage(chat_id=chat_id, text="Here are the bouldering gyms located at the East",
+        bot.sendMessage(chat_id=chat_id,
+                        text="Here are the bouldering gyms located at the East\n/places to find other gyms",
                         reply_markup=reply_markup)
 
     elif text == "West":
         keyboard = [[]]
-        all_boulder_places = json.loads(boulder_gyms)
+        global all_boulder_places
         keyboard_index = 0
         for gym_info in all_boulder_places['boulderGyms']:
             if gym_info['category'] == 'West':
@@ -124,12 +126,13 @@ def respond():
             keyboard_index = keyboard_index + 1
 
         reply_markup = telegram.ReplyKeyboardMarkup(keyboard)
-        bot.sendMessage(chat_id=chat_id, text="Here are the bouldering gyms located at the West",
+        bot.sendMessage(chat_id=chat_id,
+                        text="Here are the bouldering gyms located at the West\n/places to find other gyms",
                         reply_markup=reply_markup)
 
     elif text == "Central":
         keyboard = [[]]
-        all_boulder_places = json.loads(boulder_gyms)
+        global all_boulder_places
         keyboard_index = 0
         for gym_info in all_boulder_places['boulderGyms']:
             if gym_info['category'] == 'Central':
@@ -137,8 +140,10 @@ def respond():
             keyboard_index = keyboard_index + 1
 
         reply_markup = telegram.ReplyKeyboardMarkup(keyboard)
-        bot.sendMessage(chat_id=chat_id, text="Here are the bouldering gyms located at the Central",
+        bot.sendMessage(chat_id=chat_id,
+                        text="Here are the bouldering gyms located at the Central\n/places to find other gyms",
                         reply_markup=reply_markup)
+    # -----------------------------------------------------------------------------------------------------------------
 
     elif text == "/nearme":
         bot.sendMessage(chat_id=chat_id, text="You are at xxx now, the nearest gyms (within 10km, if any) are shown "
@@ -146,22 +151,32 @@ def respond():
 
     else:
         # for loop to find such a gym
-        # TODO: Implement function
+        global all_boulder_places
+        has_gym = False
+        for gym_info in all_boulder_places['boulderGyms']:
+            if text == gym_info['name']:
+                has_gym = True
+                caption = gym_info['name'] + "\nLocation: " + gym_info['location'] + "\nBooking: " \
+                          + gym_info['booking'] + "\nMore details: " + gym_info['url']
+                bot.sendPhoto(chat_id=chat_id, photo=gym_info['image'], caption=caption)
+
         # no such command, error message
-        bot.sendMessage(chat_id=chat_id, text=error_message)
-        try:
-            # clear the message we got from any non alphabets
-            text = re.sub(r"\W", "_", text)
-            # create the api link for the avatar based on http://avatars.adorable.io/
-            url = "https://api.adorable.io/avatars/285/{}.png".format(text.strip())
-            # reply with a photo to the name the user sent,
-            # note that you can send photos by url and telegram will fetch it for you
-            bot.sendPhoto(chat_id=chat_id, photo=url, reply_to_message_id=msg_id)
-        except Exception:
-            # if things went wrong
-            bot.sendMessage(chat_id=chat_id,
-                            text="There was a problem in the name you used, please enter different name",
-                            reply_to_message_id=msg_id)
+        if not has_gym:
+            bot.sendMessage(chat_id=chat_id, text=error_message)
+
+        # try:
+        #     # clear the message we got from any non alphabets
+        #     text = re.sub(r"\W", "_", text)
+        #     # create the api link for the avatar based on http://avatars.adorable.io/
+        #     url = "https://api.adorable.io/avatars/285/{}.png".format(text.strip())
+        #     # reply with a photo to the name the user sent,
+        #     # note that you can send photos by url and telegram will fetch it for you
+        #     bot.sendPhoto(chat_id=chat_id, photo=url, reply_to_message_id=msg_id)
+        # except Exception:
+        #     # if things went wrong
+        #     bot.sendMessage(chat_id=chat_id,
+        #                     text="There was a problem in the name you used, please enter different name",
+        #                     reply_to_message_id=msg_id)
 
     return 'ok'
 
